c5db85457e23d773ccb248591fcfcc712cdae18d
Zaah91 <zaah91@gmail.com>
2021-09-15 10:06:26 +0200

    exercise 3 done

--- src/exercise_3.java
+++ src/exercise_3.java
@@ -2,31 +2,25 @@ import java.util.Scanner;
 
 public class exercise_3 {
 
-    public static void Fermat(double a, double b, double c, double n){
-
-        System.out.println((Math.pow(a,n))+(Math.pow(b,n))==Math.pow(c,n));
-    }
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("First number:");
-        double a = scanner.nextDouble();
-        System.out.println("Second number:");
-        double b = scanner.nextDouble();
-        System.out.println("Third number");
-        double c = scanner.nextDouble();
-        System.out.println("Last number, the ^");
+        double a = 1;
+        double b = 2;
+        double c = 3;
+        System.out.println("The ^");
         double n = scanner.nextDouble();
 
-        Fermat (a,b,c,n);
-
-        /*if (x > 2 || x < 2) {
+        if (Math.pow(a,n) + Math.pow(b,n) == Math.pow(c,n) && n<=2){
             System.out.println("He was wrong");
-        } else {
-            System.out.println("Holy something, he was right");
-        }*/
+        }
+        else{
+            System.out.println("He was right");
+        }
+
+
+
     }
 }
         // Help for this!
--- src/exercise_4.java
+++ src/exercise_4.java
@@ -2,13 +2,23 @@ import java.util.Scanner;
 
 public class exercise_4 {
     public static void main(String[] args) {
-        // boolean yes = true; = true
-        // boolean no = false; = false
-        // int loVal = -999; = True
-        // int hiVal = 999; = True
-        // double grade = 87.5; = True
-        // double amount = 50.0; = True
-        // String hello = "world"; = True
+        // boolean yes = true;
+        // boolean no = false;
+        // int loVal = -999;
+        // int hiVal = 999;
+        // double grade = 87.5;
+        // double amount = 50.0;
+        // String hello = "world";
+
+        //De efterfølgende i deres rækkefølge
+        // false || true = true
+        // Error ( grundet || 50.0)
+        // true || true = true
+        // Error (grundet True (stort bogstav))
+        //
+        //
+        //
+        // grade delen = Error
 
         // Måske lavet rigtigt, dont realy know
     }
